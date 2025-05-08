fb133f373e87cf0bcedd39c0fab2e67f82304998
Kishore Reddy <kishorereddy@Kishores-MacBook-Air.local>
2025-05-08 10:38:06 +0800

    Initial commit: Project setup

--- from turtle import *.py
+++ from turtle import *.py
@@ -1,12 +1,28 @@
 import turtle
 screen=turtle.Screen()
-screen.bgcolor("White")
 pen=turtle.Turtle()
-pen.color("Blue")
-pen.speed(2)
-pen.forward(100)
-pen.right(90)
-pen.forward(300)
-pen.right(90)
+pen.speed(10)
+length = int(input('length of loop: '))
+
+numbers = []
+
+for ii in range(length):
+  new_number = input("Please input your "+str(ii+1)+" number.")
+  numbers.append(new_number)
+
+#print(numbers)
+
+def step_any_loop(numbers, repeats):
+#draw line
+
+ n = len(numbers)
+
+ for i in range(repeats):
+    for i in range(n):
+      pen.forward(numbers[i] * 100)
+      pen.right(90)
+step_any_loop( [3, 1, 2, 1], 4)
+#hide turtle 
 pen.hideturtle()
 turtle.done()
+#keep turtle window open when done
